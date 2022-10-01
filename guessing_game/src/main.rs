aee2ef08d8c41c1fb8313d797094d8646f22452a
Adnan Taufique <44573741+adnan33@users.noreply.github.com>
2022-10-01 23:29:32 +0600

    Modified the game to have quit prompt

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,35 +1,52 @@
+#![allow(unused,unused_variables,while_true)]
+
+use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
-use std::io;
 
 fn main() {
-    println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
 
-    loop {
-        println!("Please input your guess.");
-
+    while true
+    {  
+        let mut flag= String::new();
+        println!("Guess The Number between 1 to 30!.");
+        println!("Please input a number:");
+    
+        let secret_number = rand::thread_rng().gen_range(1..=30);
+    
         let mut guess = String::new();
-
+    
         io::stdin()
             .read_line(&mut guess)
-            .expect("Failed to read line");
-
+            .expect("Read line failed.");
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-
         println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+    
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You WIN!!!");
                 break;
-            }
+        },
+        }
+        println!("the number was: {secret_number}");
+        println!("Press enter to continue, 'q' to quit!");
+
+        io::stdin()
+            .read_line(&mut flag)
+            .expect("Read line failed.");
+
+        match flag.trim(){
+            "q" => break,
+            _ => continue,
         }
     }
+
+    
 }
