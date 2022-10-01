be9620fccc2e3c5f95b42fd3c3c4d155fa447b2c
Adnan Taufique <44573741+adnan33@users.noreply.github.com>
2022-10-01 22:06:31 +0600

    Completed the game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,28 @@
 #![allow(unused,unused_variables)]
 
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Guess The Number!.");
+
+    println!("Guess The Number between 1 to 30!.");
     println!("Please input a number:");
+
+    let secret_number = rand::thread_rng().gen_range(1..=30);
 
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Read line failed.");
-
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too Small!"),
+        Ordering::Greater => println!("Too Big"),
+        Ordering::Equal => println!("You WIN!!!"),
+    }
+    println!("the number was: {secret_number}");
 }
